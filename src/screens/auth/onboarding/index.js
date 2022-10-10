a84a69d81acb48dd386637c284ed7e1c0663f782
PhuocTri0106 <you@PhuocTri0106.com>
2022-10-10 16:32:48 +0700

    update onboarding screen

--- src/screens/auth/onboarding/index.js
+++ src/screens/auth/onboarding/index.js
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, Dimensions, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { IC_Binoculars } from '../../../assets/icons'
+import { IC_Binoculars, IC_Calendar, IC_Delivery, IC_MacApple, IC_RestaurantMenu } from '../../../assets/icons'
 import scale from '../../../utils/responsive'
 
-const {width: screenWidth} = Dimensions.get('window');  
+const {width: screenWidth} = Dimensions.get('screen');  
 
 const OnboardingScreen = () => {
      const [viewActive, setViewActive] = useState(0);
@@ -23,42 +23,53 @@ const OnboardingScreen = () => {
         const views = [
             { 
                 view: 
-                <View style={{width: screenWidth, height:'100%'}}>
+                <View key={'0'} style={{width: 411.42857142857144, height:'100%', borderWidth:1}}>
+                    <IC_RestaurantMenu style={styles.icon}/>
                     <Text  style={styles.titleText}>Browse  Food</Text>
                     <Text style={styles.subText}>{"Welcome to our restaurant app! Log in\n and check out our delicious food."}</Text>
                 </View>,
+                key: 0,
             },
             { 
                 view: 
-                <View style={{width: screenWidth, height:'100%'}}>
+                <View key={'1'} style={{width: 411.42857142857144, height:'100%', borderWidth:1}}>
+                    <IC_Delivery style={styles.icon}/>
                     <Text  style={styles.titleText}>Order Food</Text>
                     <Text style={styles.subText}>{"Hungry? Order food in just a few clicks\nand we'll take care of you."}</Text>
                 </View>,
+                key: 1,
             },
             { 
                 view: 
-                <View style={{width: screenWidth, height:'100%'}}>
+                <View key={'2'} style={{width: 411.42857142857144, height:'100%', borderWidth:1}}>
+                    <IC_Calendar style={styles.icon}/>
                     <Text  style={styles.titleText}>Make Reservations</Text>
                     <Text style={styles.subText}>{"Book a table in advance to avoid\nwaiting in line."}</Text>
                 </View>,
+                key: 2,
             },
             { 
                 view: 
-                <View style={{width: screenWidth, height:'100%'}}>
+                <View key={'3'} style={{width: 411.42857142857144, height:'100%', borderWidth:1}}>
+                    <IC_Binoculars style={styles.icon}/>
                     <Text  style={styles.titleText}>Quick Search</Text>
                     <Text style={styles.subText}>{"Quickly find food items you like\nthe most."}</Text>
                 </View>,
+                key: 3,
             },
             { 
                 view: 
-                <View style={{width: screenWidth, height:'100%'}}>
+                <View key={'4'} style={{width: 411.42857142857144, height:'100%', borderWidth:1}}>
+                    <IC_MacApple style={styles.icon}/>
                     <Text  style={styles.titleText}>Apple Pay</Text>
                     <Text style={styles.subText}>{"We know you're busy, so you can pay\nwith your phone in just one click."}</Text>
                     <TouchableOpacity style={styles.skip}>
                         <Text style={styles.skipText}>Skip</Text>
                     </TouchableOpacity>
                 </View>,
-            }
+                key: 4,
+            },
+          
     ]
     setViewList(views);
     }, [])
@@ -70,18 +81,14 @@ const OnboardingScreen = () => {
             horizontal
             pagingEnabled
             showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{width: screenWidth * viewList.length, height:812}}
+            contentContainerStyle={{width: 411.42857142857144 * 5, height:812}}
         >
-            {viewList.map((e,index) =>
-                <View key={index.toString()}>
-                    {e.view}
-                </View> 
-            )}
+            {viewList.map((e) => e.view)}
         </ScrollView>
         <View style={styles.wrapDot}>
             {viewList.map((e, index) => (
               <Text
-                key={e}
+                key={e.key}
                 style={viewActive == index ? styles.dotActive : styles.dot}>
                 ●
               </Text>
@@ -105,6 +112,7 @@ const styles = StyleSheet.create({
         letter: -0.5,
         alignSelf: 'center',
         justifyContent: 'center',
+        color: 'white',
     },
     subText: {
         fontWeight: '400',
@@ -113,6 +121,7 @@ const styles = StyleSheet.create({
         letter: -0.47,
         alignSelf: 'center',
         justifyContent: 'center',
+        color: 'white',
     },
     wrapDot: {
         position: 'absolute',
@@ -139,13 +148,15 @@ const styles = StyleSheet.create({
         fontSize: scale(17),
         alignSelf: 'center',
         justifyContent: 'center',
+        color: 'white'
     },
-    IC_Binoculars: {
+    icon: {
         position: 'absolute',
         top: 194,
         width: scale(113),
         height: 96.37,
         justifyContent: 'center',
         alignSelf: 'center',
+        borderColor: '#ffffff',
     }
 })

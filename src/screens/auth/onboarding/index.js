464597750889986dc55c966a39ac50a527a98c59
PhuocTri0106 <you@PhuocTri0106.com>
2022-10-11 12:13:40 +0700

    finish Onboarding Screen

--- src/screens/auth/onboarding/index.js
+++ src/screens/auth/onboarding/index.js
@@ -1,67 +1,73 @@
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, Dimensions, TouchableOpacity, Image } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import {IMG_Binoculars,IMG_Calendar,IMG_Delivery,IMG_MacApple,IMG_RestaurantMenu} from '../../../assets/images'
 import scale from '../../../utils/responsive'
 import FONT_FAMILY from '../../../constants/fonts'
 import { CUSTOM_COLOR } from '../../../constants/color'
 
-const {width: screenWidth} = Dimensions.get('screen');  
+const {width: screenWidth} = Dimensions.get('window');  
 
 const OnboardingScreen = () => {
-     const [viewActive, setViewActive] = useState(0);
+        const [viewList, setViewList] = useState([]);
+        const [currentView, setCurrentView] = useState(0);
+        const stepCarousel = useRef(null);
 
-  onchange = nativeEvent => {
-    if (nativeEvent) {
-      const slide = Math.ceil(
-        nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
-      );
-      if (slide != viewActive) {
-        setViewActive(slide);
-      }
+  const handleScroll = (e) => {
+    if (!e) {
+        return;
+    }
+    const {nativeEvent} = e;
+    if(nativeEvent && nativeEvent.contentOffset) {
+        const currentOffset = nativeEvent.contentOffset.x;
+        let viewIndex = 0;
+        if(nativeEvent.contentOffset.x > 0) {
+            viewIndex = Math.floor((nativeEvent.contentOffset.x + screenWidth / 2) / screenWidth);
+        }
+        setCurrentView(viewIndex);
     }
   };
-    const [viewList, setViewList] = useState([]);
+
     useEffect(() => {
         const views = [
             { 
                 view: 
-                <View key={'0'} style={{width: 411.42857142857144, height:'100%'}}>
+                <View style={{width: screenWidth, height:'100%'}}>
                     <Image source={IMG_RestaurantMenu} style={styles.icon}/>
                     <Text  style={styles.titleText}>Browse  Food</Text>
                     <Text style={styles.subText}>{"Welcome to our restaurant app! Log in\n and check out our delicious food."}</Text>
                 </View>,
-                number: 0,
+                id:0,
             },
             { 
                 view: 
-                <View key={'1'} style={{width: 411.42857142857144, height:'100%'}}>
+                <View  style={{width: screenWidth, height:'100%'}}>
                     <Image source={IMG_Delivery} style={styles.icon}/>
                     <Text  style={styles.titleText}>Order Food</Text>
                     <Text style={styles.subText}>{"Hungry? Order food in just a few clicks\nand we'll take care of you."}</Text>
                 </View>,
-                number: 1,
+                id:1,
             },
             { 
                 view: 
-                <View key={'2'} style={{width: 411.42857142857144, height:'100%'}}>
+                <View style={{width: screenWidth, height:'100%'}}>
                     <Image source={IMG_Calendar} style={styles.icon}/>
                     <Text  style={styles.titleText}>Make Reservations</Text>
                     <Text style={styles.subText}>{"Book a table in advance to avoid\nwaiting in line."}</Text>
                 </View>,
-                number: 2,
+                id:2,
             },
             { 
                 view: 
-                <View key={'3'} style={{width: 411.42857142857144, height:'100%'}}>
+                <View  style={{width: screenWidth, height:'100%'}}>
                     <Image source={IMG_Binoculars} style={styles.icon}/>
                     <Text  style={styles.titleText}>Quick Search</Text>
                     <Text style={styles.subText}>{"Quickly find food items you like\nthe most."}</Text>
                 </View>,
-                number: 3,
+                id:3,
             },
             { 
                 view: 
-                <View key={'4'} style={{width: 411.42857142857144, height:'100%'}}>
+                <View  style={{width: screenWidth, height:'100%'}}>
                     <Image source={IMG_MacApple} style={styles.icon}/>
                     <Text  style={styles.titleText}>Apple Pay</Text>
                     <Text style={styles.subText}>{"We know you're busy, so you can pay\nwith your phone in just one click."}</Text>
@@ -69,33 +75,55 @@ const OnboardingScreen = () => {
                         <Text style={styles.skipText}>Skip</Text>
                     </TouchableOpacity>
                 </View>,
-                number: 4,
+                id:4,
             },
           
     ]
     setViewList(views);
     }, [])
     
+    useEffect(() => {
+        if(viewList.length > 0) {
+            let index = 0;
+            setInterval(() => {
+                stepCarousel.current.scrollTo({ x: index * screenWidth, y:0, animated:true});
+                index += 1;
+                // if(index === viewList.length) {
+                //     index = 0;
+                //     //ADJUST NAVIGATE HERE-------------------
+                // }
+            }, 3000);
+        }
+    }, [viewList])
+
   return (
     <SafeAreaView style={styles.container}>
         <ScrollView
-            onScroll={({nativeEvent}) => onchange(nativeEvent)}
+            onScroll={handleScroll}
+            scrollEventThrottle={16}
+            ref={stepCarousel}
             horizontal
             pagingEnabled
             showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{width: 411.42857142857144 * 5, height:812}}
+            contentContainerStyle={{width: screenWidth * viewList.length, height:812}}
         >
-            {viewList.map((e) => e.view)}
+            {viewList.map((e, index) => 
+                <View key={index.toString()}>
+                    {e.view}
+                </View>            
+            )}
         </ScrollView>
         <View style={styles.wrapDot}>
-            {viewList.map((e, index) => (
+            {viewList.map((e,index) => (
               <Text
-                key={index}
-                style={viewActive == index ? styles.dotActive : styles.dot}>
+                key={index.toString()}
+                style={currentView == index ? styles.dotActive : styles.dot}
+                >
                 ●
               </Text>
             ))}
         </View>
+
     </SafeAreaView>
   )
 }
@@ -138,7 +166,7 @@ const styles = StyleSheet.create({
     },
     dot: {
         margin: 3,
-        opacity: 0.1,
+        opacity: 0.27,
         color: CUSTOM_COLOR.White,
     },
     skip: {

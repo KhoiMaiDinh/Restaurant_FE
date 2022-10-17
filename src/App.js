4ccb01d86b3b33821f01102dd9527c1302c57d06
PhuocTri0106 <you@PhuocTri0106.com>
2022-10-17 14:38:31 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import RootNavigator from './routes/RootNavigator';
-import OrdersScreen from './screens/home/ordersScreen';
+
 
 const App = props => {
   return <RootNavigator {...props} />;

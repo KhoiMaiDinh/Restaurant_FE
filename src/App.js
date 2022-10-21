031e8cdfe666b555dcdcefe5519704fb7ae4b99c
2122059 <21522059@gm.uit.edu.vn>
2022-10-21 12:43:51 +0700

    pull code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import OnboardingScreen from './screens/auth/onboarding'
+import React from 'react';
+import RootNavigator from './routes/RootNavigator';
+import MenuScreen from './screens/home/menuScreen';
+import ImageTab from './screens/home/menuScreen/components/imageTab';
+import OrdersScreen from './screens/home/ordersScreen/index'
 
-const App = () => {
-  return (
-      <OnboardingScreen/>
-  )
-}
+const App = props => {
+  return <MenuScreen/>;
+};
 
 export default App
 

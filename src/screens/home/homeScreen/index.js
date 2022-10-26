9f5de46621ce28fb0bab149789936ebd293a8467
PhuocTri0106 <you@PhuocTri0106.com>
2022-10-26 15:03:30 +0700

    add skeleton

--- src/screens/home/homeScreen/index.js
+++ src/screens/home/homeScreen/index.js
@@ -13,15 +13,22 @@ import CircularCategories from './components/circularCategory';
 import FONT_FAMILY from '../../../constants/fonts';
 import MostPopular from './components/mostPopular';
 import BestDeals from './components/bestDeals';
+import Skeleton from '../../skeleton';
+
 
 const HomeScreen = props => {
   const [categoryData, setCategoryData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const getCategory = () => {
     const categoryURL =
       'https://restaurant-uit-server.herokuapp.com/category/popular/';
     return fetch(categoryURL)
       .then(res => res.json())
-      .then(json => setCategoryData(json.categories));
+      .then(json => {setCategoryData(json.categories);
+        if(loading) {
+          setLoading(false);
+        }
+      });
   };
 
   const [bestFoodData, setBestFoodData] = useState([]);
@@ -30,7 +37,11 @@ const HomeScreen = props => {
       'https://restaurant-uit-server.herokuapp.com/food/best-deals/';
     return fetch(bestFoodURL)
       .then(res => res.json())
-      .then(json => setBestFoodData(json.foods));
+      .then(json => {setBestFoodData(json.foods);
+        if(loading) {
+          setLoading(false);
+        }
+      });
   };
 
   const [foodData, setFoodData] = useState([]);
@@ -38,36 +49,46 @@ const HomeScreen = props => {
     const foodURL = 'https://restaurant-uit-server.herokuapp.com/food/popular/';
     return fetch(foodURL)
       .then(res => res.json())
-      .then(json => setFoodData(json.foods));
+      .then(json => {setFoodData(json.foods);
+        if(loading) {
+          setLoading(false);
+        }
+      });
   };
 
   useEffect(() => {
     getCategory();
     getBestFood();
     getPopularFood();
+    
   }, []);
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{flex: 1}}>
-        <ScrollView style={styles.scrollView}>
-          <View style={styles.viewSecond}>
-            <Text style={styles.categoryText}>Danh mục nổi bật</Text>
-            <CircularCategories
-              style={styles.categoryRow}
-              categoryData={categoryData}
-              {...props}
-            />
-            <Text style={styles.dealText}>Best Deals</Text>
-            <BestDeals
-              style={styles.bestDeal}
-              foodData={bestFoodData}
-              {...props}
-            />
-            <Text style={styles.dealText}>Nhiều người đã thử, bạn có muốn?</Text>
-            <MostPopular foodData={foodData} {...props} />
-          </View>
-        </ScrollView>
-      </View>
+      {loading ? (
+      <Skeleton/>
+      ):(
+        <View style={{flex: 1}}>
+          <ScrollView style={styles.scrollView}>
+            <View style={styles.viewSecond}>
+              <Text style={styles.categoryText}>Danh mục nổi bật</Text>
+              <CircularCategories
+                style={styles.categoryRow}
+                categoryData={categoryData}
+                {...props}
+              />
+              <Text style={styles.dealText}>Best Deals</Text>
+              <BestDeals
+                style={styles.bestDeal}
+                foodData={bestFoodData}
+                {...props}
+              />
+              <Text style={styles.dealText}>Nhiều người đã thử, bạn có muốn?</Text>
+              <MostPopular foodData={foodData} {...props} />
+            </View>
+          </ScrollView>
+        </View>
+      )
+      }   
     </SafeAreaView>
   );
 };

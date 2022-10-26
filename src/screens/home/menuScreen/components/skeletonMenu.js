05bdaebee12757ebe0f23ead3bace16a75805e97
PhuocTri0106 <you@PhuocTri0106.com>
2022-10-26 16:56:02 +0700

    add skeleton menu

--- src/screens/home/menuScreen/components/skeletonMenu.js
+++ src/screens/home/menuScreen/components/skeletonMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image, Text, ScrollView, Dimensions, StyleSheet} from 'react-native';
+import {View, ScrollView, Dimensions, StyleSheet} from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 import { CUSTOM_COLOR } from '../../../../constants/color';
 import scale from '../../../../utils/responsive';
@@ -9,9 +9,18 @@ const SkeletonMenu = () => {
     <ScrollView>
         <SkeletonPlaceholder style={styles.container}>
             <View style={{flex: 1}}>
-                <View style={{marginTop:80, alignSelf:'center', width: scale(335), height: 180}}/>
-                <View style={{marginTop:80, alignSelf:'center', width: scale(335), height: 180}}/>
-                
+                <View style={{flexDirection: 'row',marginTop:45 }}>
+                    <View style={{ marginLeft: scale(14), width: scale(165), height: 115}}/>
+                    <View style={{ marginLeft: scale(35), width: scale(165), height: 115}}/>
+                </View>
+                <View style={{flexDirection: 'row',marginTop:125 }}>
+                    <View style={{ marginLeft: scale(14), width: scale(165), height: 115}}/>
+                    <View style={{ marginLeft: scale(35), width: scale(165), height: 115}}/>
+                </View>
+                <View style={{flexDirection: 'row',marginTop:125 }}>
+                    <View style={{ marginLeft: scale(14), width: scale(165), height: 115}}/>
+                    <View style={{ marginLeft: scale(35), width: scale(165), height: 115}}/>
+                </View>
             </View>
         </SkeletonPlaceholder>
     </ScrollView>

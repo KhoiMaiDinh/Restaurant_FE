a439caefc50b335115638ec3d2769a820c7c4859
PhuocTri0106 <you@PhuocTri0106.com>
2022-11-30 21:20:22 +0700

    no message

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -18,26 +18,94 @@ import {useDispatch} from 'react-redux';
 import {login} from '../../../features/auth/userSlice';
 import axios from 'axios';
 import {BASE_URL} from '../../../utils/api';
+import { disabled } from 'deprecated-react-native-prop-types/DeprecatedTextPropTypes';
 
 const LoginScreen = props => {
   const dispatch = useDispatch();
   const {navigation} = props;
   const [email, setMail] = useState('');
   const [password, setPass] = useState('');
+  const [checkValidEmail, setCheckValidEmail] = useState(false);
+  const [checkValidPassword, setCheckValidPassword] = useState(false);
 
-  const handleLogin = async () => {
-    try {
-      const response = await axios.post(`${BASE_URL}/auth/login`, {
-        email: email,
-        password: password,
-      });
-      dispatch(login(response.data));
-      navigation.navigate('AppStackScreen');
-    } catch (error) {
-      console.log(error);
+  
+  const handleCheckEmail = text => {
+    let re = /\S+@\S+\.\S+/;
+    let regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    setMail(text);
+    if (re.test(text) || regex.test(text)) {
+      setCheckValidEmail(false);
+    } else {
+      setCheckValidEmail(true);
+    }
+  };
+  const handleCheckPassword = text =>{
+    setPass(text);
+    if(checkPasswordValidity(text)){
+      setCheckValidPassword(false);
+    }
+    else {
+      setCheckValidPassword(true);
     }
   };
 
+  const checkPasswordValidity = value => {
+    
+    const isNonWhiteSpace = /^\S*$/;
+    if (!isNonWhiteSpace.test(value)) {
+      return 'Mật khẩu không được chứa khoảng trắng.';
+    }
+
+    const isContainsUppercase = /^(?=.*[A-Z]).*$/;
+    if (!isContainsUppercase.test(value)) {
+      return 'Password must have at least one Uppercase Character.';
+    }
+
+    const isContainsLowercase = /^(?=.*[a-z]).*$/;
+    if (!isContainsLowercase.test(value)) {
+      return 'Password must have at least one Lowercase Character.';
+    }
+
+    const isContainsNumber = /^(?=.*[0-9]).*$/;
+    if (!isContainsNumber.test(value)) {
+      return 'Password must contain at least one Digit.';
+    }
+
+    const isValidLength = /^.{8,16}$/;
+    if (!isValidLength.test(value)) {
+      return 'Password must be 8-16 Characters Long.';
+    }
+
+    const isContainsSymbol =
+      /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+    if (!isContainsSymbol.test(value)) {
+      return 'Password must contain at least one Special Symbol.';
+    }
+
+    return null;
+  };
+  let validPassWordText = null;
+  const handleLogin = async () => {
+    const checkPassword = checkPasswordValidity(password);
+    if(!checkPassword)
+    {
+      try {
+        const response = await axios.post(`${BASE_URL}/auth/login`, {
+          email: email.toLocaleLowerCase(),
+          password: password,
+        });
+        dispatch(login(response.data));
+        navigation.navigate('AppStackScreen');
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    }
+     else{
+      validPassWordText = checkPassword;
+    } 
+  };
   return (
     <TouchableWithoutFeedback
       onPress={() => Keyboard.dismiss() && TextInput.clearFocus()}>
@@ -48,35 +116,60 @@ const LoginScreen = props => {
           <IC_GoBack />
         </TouchableOpacity>
         <View style={styles.tittleBox}>
-          <Text style={styles.screenTittle}>Sign In</Text>
+          <Text style={styles.screenTittle}>Log In</Text>
         </View>
         <View style={styles.inputMailBox}>
           <TextInput
-            onChangeText={email => setMail(email)}
+            onChangeText={text => handleCheckEmail(text)}
             placeholderTextColor={CUSTOM_COLOR.Grey}
             placeholder="Email address"
+            value={email}
             style={styles.inputText}
             keyboardType="email-address"
           />
+          {checkValidEmail ? (
+          <Text style={styles.emailFailed}>Sai định dạng email. VD:"abc@xyz"</Text>
+        ) : (
+          <Text style={styles.emailFailed}> </Text>
+        )}
         </View>
+        
         <View style={styles.inputPasswordBox}>
           <TextInput
             secureTextEntry={true}
-            onChangeText={password => setPass(password)}
+            onChangeText={text => setPass(text)}
             placeholderTextColor={CUSTOM_COLOR.Grey}
+            value={password}
             placeholder="Password"
             style={styles.inputText}
           />
+          
+          {/* {checkValidPassword ? (
+          <Text style={styles.textFailed}>{checkPasswordValidity(password)}</Text>
+        ) : ( */}
+          <Text style={styles.passwordFailed}> {validPassWordText}</Text>
+        {/* )} */}
         </View>
+        
+        {email == '' || password == '' || checkValidEmail == true ? (
         <TouchableOpacity
+          disabled
           style={styles.loginButtonBoxPosition}
-          onPress={() => {
-            handleLogin();
-            }}>
+          onPress={() =>
+            {handleLogin()}}>
           <View style={styles.loginButtonBox}>
             <Text style={styles.buttonText}>Login</Text>
           </View>
         </TouchableOpacity>
+        ) : (<TouchableOpacity
+          style={styles.loginButtonBoxPosition}
+          onPress={() =>
+            handleLogin()}>
+          <View style={styles.loginButtonBox}>
+            <Text style={styles.buttonText}>Login</Text>
+          </View>
+        </TouchableOpacity>
+        )} 
         <Text style={styles.orText}>OR</Text>
         <TouchableOpacity style={styles.FBLoginButtonBoxPosition}>
           <View style={styles.FBLoginButtonBox}>
@@ -86,7 +179,7 @@ const LoginScreen = props => {
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
-};
+}
 
 export default LoginScreen;
 
@@ -97,13 +190,13 @@ const styles = StyleSheet.create({
   },
   goBackButton: {
     position: 'absolute',
-    left: scale(9),
-    top: scale(50),
+    marginLeft: scale(9),
+    marginTop: scale(50),
   },
   tittleBox: {
     position: 'absolute',
-    top: scale(128),
-    left: scale(25),
+    marginTop: scale(128),
+    marginLeft: scale(25),
   },
   screenTittle: {
     color: CUSTOM_COLOR.Primary,
@@ -112,7 +205,7 @@ const styles = StyleSheet.create({
   },
   inputMailBox: {
     position: 'absolute',
-    top: scale(191),
+    marginTop: scale(191),
     alignSelf: 'center',
     height: scale(53),
     width: scale(323),
@@ -121,14 +214,14 @@ const styles = StyleSheet.create({
     borderRadius: 26.5,
   },
   inputText: {
-    left: scale(17),
+    marginLeft: scale(17),
     color: CUSTOM_COLOR.Black,
     width: scale(299),
     fontFamily: FONT_FAMILY.NexaRegular,
   },
   inputPasswordBox: {
     position: 'absolute',
-    top: scale(267),
+    marginTop: scale(267),
     alignSelf: 'center',
     height: scale(53),
     width: scale(323),
@@ -138,7 +231,7 @@ const styles = StyleSheet.create({
   },
   loginButtonBoxPosition: {
     position: 'absolute',
-    top: scale(343),
+    marginTop: scale(343),
     alignSelf: 'center',
   },
   loginButtonBox: {
@@ -153,12 +246,12 @@ const styles = StyleSheet.create({
     color: CUSTOM_COLOR.Black,
     fontFamily: FONT_FAMILY.NexaRegular,
     fontSize: scale(17),
-    top: scale(445),
+    marginTop: scale(450),
     alignSelf: 'center',
   },
   FBLoginButtonBoxPosition: {
     position: 'absolute',
-    top: scale(516),
+    marginTop: scale(516),
     alignSelf: 'center',
   },
   FBLoginButtonBox: {
@@ -172,5 +265,21 @@ const styles = StyleSheet.create({
   buttonText: {
     color: CUSTOM_COLOR.White,
     fontFamily: FONT_FAMILY.NexaRegular,
+  },
+  emailFailed: {
+    marginLeft: scale(70),
+    paddingTop: scale(245),
+    alignSelf:'flex-start',
+    fontFamily: FONT_FAMILY.NexaRegular,
+    fontSize: scale(12),
+    color: 'red',
+  },
+  passwordFailed: {
+    marginLeft: scale(70),
+    paddingTop: scale(275),
+    alignSelf:'flex-start',
+    fontFamily: FONT_FAMILY.NexaRegular,
+    fontSize: scale(12),
+    color: 'red',
   },
 });

3501ad29ed178fd7bd8a402980476ae0303b2e90
PhuocTri0106 <you@PhuocTri0106.com>
2022-12-09 11:58:57 +0700

    Add function choose image in Edit Profile Screen

--- src/screens/home/myProfile/editProfileScreen/index.js
+++ src/screens/home/myProfile/editProfileScreen/index.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Image, Text, View, SafeAreaView, TouchableOpacity, TextInput, TouchableWithoutFeedback, Dimensions, Keyboard, ScrollView, KeyboardAvoidingView } from 'react-native'
+import { StyleSheet, Image, Text, View, SafeAreaView, TouchableOpacity, TextInput, TouchableWithoutFeedback, Dimensions, Keyboard, ScrollView, KeyboardAvoidingView, ImageBackground } from 'react-native'
 import React, {useState} from 'react'
 import {CUSTOM_COLOR} from '../../../../constants/color';
 import {IC_GoBack} from '../../../../assets/icons/index';
 import scale from '../../../../utils/responsive';
 import FONT_FAMILY from '../../../../constants/fonts';
 import { IMG_LisaAvatar } from '../../../../assets/images';
+import ImagePicker from 'react-native-image-crop-picker';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+
 
 const EditProfileScreen = () => {
     const [email, setEmail] = useState('');
@@ -33,18 +37,89 @@ const EditProfileScreen = () => {
           setCheckValidNumber(true);
         }
       };
+      const [image, setImage] = useState('https://drive.google.com/file/d/1Snr93Bao8zv0tBTNONR7T2NpRs_gTtkz/view?usp=share_link');
+      const takePhotoFromCamera = () => {
+        ImagePicker.openCamera({
+          compressImageMaxWidth: scale(300),
+          compressImageMaxHeight: scale(300),
+          cropping: true,
+          compressImageQuality: 0.7
+        }).then(image => {
+          console.log(image);
+          setImage(image.path);
+          this.bs.current.snapTo(1);
+        });
+      }
+      const choosePhotoFromLibrary = () => {
+        ImagePicker.openPicker({
+          width: scale(300),
+          height: scale(300),
+          cropping: true,
+          compressImageQuality: 0.7
+        }).then(image => {
+          console.log(image);
+          setImage(image.path);
+          this.bs.current.snapTo(1);
+        });
+      }
+      renderInner = () => (
+        <View style={stylePanel.panel}>
+          <View style={{alignItems: 'center'}}>
+            <Text style={stylePanel.panelTitle}>Upload Photo</Text>
+            <Text style={stylePanel.panelSubtitle}>Choose Your Profile Picture</Text>
+          </View>
+          <TouchableOpacity style={stylePanel.panelButton} onPress={takePhotoFromCamera}>
+            <Text style={stylePanel.panelButtonTitle}>Take Photo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={stylePanel.panelButton} onPress={choosePhotoFromLibrary}>
+            <Text style={stylePanel.panelButtonTitle}>Choose From Library</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={stylePanel.panelButton}
+            onPress={() => this.bs.current.snapTo(1)}>
+            <Text style={stylePanel.panelButtonTitle}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+      );
+      renderHeader = () => (
+        <View style={stylePanel.header}>
+          <View style={stylePanel.panelHeader}>
+            <View style={stylePanel.panelHandle} />
+          </View>
+        </View>
+      );
+    
+      bs = React.createRef();
+      fall = new Animated.Value(1);
+
   return (
     <ScrollView>
         <SafeAreaView style={styles.container}>
+        <BottomSheet
+        ref={this.bs}
+        snapPoints={[330, 0]}
+        renderContent={this.renderInner}
+        renderHeader={this.renderHeader}
+        initialSnap={1}
+        callbackNode={this.fall}
+        enabledGestureInteraction={true}
+      />
+      <Animated.View style={{margin: 20,
+        opacity: Animated.add(0.1, Animated.multiply(this.fall, 1.0)),
+    }}>
             {/* Avatar */}
             <>
                 <TouchableOpacity style={styles.avatarTouch} >
-                    <Image style={styles.avatar} source={IMG_LisaAvatar}/>
+                    <Image  source={{
+                    uri: image,
+                    }}
+                    style={{height: '100%', width: '100%', borderRadius: scale(360)}}
+                    />
                 </TouchableOpacity>
             </>
             {/* Edit Profile Picture */}
             <>
-                <TouchableOpacity style={styles.editProfilePictureTouch}>
+                <TouchableOpacity style={styles.editProfilePictureTouch} onPress={() => this.bs.current.snapTo(0)} >
                     <Text style={styles.editProfilePictureText} >
                         Edit Profile Picture
                     </Text>
@@ -116,7 +191,7 @@ const EditProfileScreen = () => {
                         </View>
                             <View style={{marginTop: scale(30)}}>
                                 {checkValidEmail ? (
-                                    <Text style={styles.textFailed}>Sai định dạng email. VD:"abc@xyz.mnp..."</Text>
+                                    <Text style={styles.textFailed}>Sai định dạng email. VD:"user@gmail.com"</Text>
                                 ) : (
                                     <Text style={styles.textFailed}> </Text>
                                 )}
@@ -166,13 +241,83 @@ const EditProfileScreen = () => {
                     <Text style={styles.buttonText}>Lưu</Text>
                 </TouchableOpacity>
             </>
+            </Animated.View>
         </SafeAreaView>
     </ScrollView>
   )
 }
 
 export default EditProfileScreen
-
+const stylePanel = StyleSheet.create({panel: {
+    padding: scale(20),
+    backgroundColor: CUSTOM_COLOR.White,
+    paddingTop: scale(20),
+},
+commandButton: {
+    padding: scale(15),
+    borderRadius: scale(10),
+    backgroundColor: CUSTOM_COLOR.White,
+    alignItems: 'center',
+    marginTop: scale(10),
+  },
+  header: {
+    backgroundColor: CUSTOM_COLOR.Primary,
+    shadowColor: CUSTOM_COLOR.White,
+    shadowOffset: {width: -1, height: -3},
+    shadowRadius: scale(2),
+    shadowOpacity: 0.4,
+    paddingTop: scale(20),
+    borderTopLeftRadius: scale(20),
+    borderTopRightRadius: scale(20),
+  },
+  panelHeader: {
+    alignItems: 'center',
+  },
+  panelHandle: {
+    width: scale(40),
+    height: scale(8),
+    borderRadius: scale(4),
+    backgroundColor: CUSTOM_COLOR.White,
+    marginBottom: scale(10),
+  },
+  panelTitle: {
+    fontSize: 27,
+    color: CUSTOM_COLOR.Primary,
+    height: scale(35),
+  },
+  panelSubtitle: {
+    fontSize: scale(14),
+    color: CUSTOM_COLOR.Primary,
+    height: scale(30),
+    marginBottom: scale(10),
+  },
+  panelButton: {
+    padding: scale(13),
+    borderRadius: scale(10),
+    backgroundColor: CUSTOM_COLOR.Primary,
+    alignItems: 'center',
+    marginVertical: scale(7),
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    color: CUSTOM_COLOR.White,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: scale(10),
+    marginBottom: scale(10),
+    borderBottomWidth: scale(1),
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: scale(5),
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: scale(10),
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: scale(5),
+  },
+});
 const styles = StyleSheet.create({
     container: {
         height: Dimensions.get('window').height,
@@ -183,7 +328,7 @@ const styles = StyleSheet.create({
         width:scale(110),
         height:scale(110),
         borderRadius: 360,
-        marginTop: scale(20),
+        marginTop: scale(5),
         alignSelf: 'center',
     },
     editProfilePictureTouch: {
@@ -200,7 +345,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         marginTop: scale(20),
         height: scale(80),
-        width: Dimensions.get('window').width,
+        width: '100%',
+        justifyContent: 'space-between',
     },
     editProfilePictureText: {
         fontFamily: FONT_FAMILY.NexaLight,
@@ -221,12 +367,11 @@ const styles = StyleSheet.create({
         fontSize: scale(25),
         color: CUSTOM_COLOR.Primary,
         textAlign: 'center',
-        // marginTop: scale(10),
     },
     NameInput: {
-        width: scale(200),
+        width: scale(160),
         height: scale(50),
-        paddingLeft: scale(10),   
+        //paddingLeft: scale(10),   
     },
     emailInput: {
         width: scale(345),

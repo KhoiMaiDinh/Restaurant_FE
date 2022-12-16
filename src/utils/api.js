bbeb1ea4cc5e048b6085b83d705b3f70476592cc
PhuocTri0106 <you@PhuocTri0106.com>
2022-12-16 10:45:14 +0700

    update API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://172.20.22.73:8080';
-//export const BASE_URL = 'https://restaurant-uit-server.onrender.com';
+//export const BASE_URL = 'http://172.20.22.73:8080';
+export const BASE_URL = 'https://restaurant-uit-server.onrender.com';

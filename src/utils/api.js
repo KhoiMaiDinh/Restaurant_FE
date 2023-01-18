169a5140eb3fc4310a6f8a9585fbaf5141d3d833
PhuocTri0106 <you@PhuocTri0106.com>
2023-01-18 11:02:07 +0700

    no message

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = 'http://172.30.61.144:8080';
+//export const BASE_URL = 'http://192.168.1.104:8080';
 export const BASE_URL = 'https://restaurant-uit-server.onrender.com/';
